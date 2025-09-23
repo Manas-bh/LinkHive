0aa5630e935c960d7a693a3fb4fe01e30df1f7d5
NoIdea2001 <saumyabhaintwal@gmail.com>
2025-09-23 11:11:40 +0530

    joined home to signup and fixed some signup front end

--- components/CreateAcc.tsx
+++ components/CreateAcc.tsx
@@ -1,20 +1,29 @@
-import Image from 'next/image'
-import React from 'react'
-import people from '@/public/people.svg'
+import Image from "next/image";
+import React from "react";
+import people from "@/public/people.svg";
+import Link from "next/link";
 
 const CreateAcc = ({ className = "" }) => {
   return (
-    <div className={`w-[8rem] md:w-[9rem] lg:w-[10.5rem] flex items-center ${className}`}>
+    <div
+      className={`w-[8rem] md:w-[9rem] lg:w-[10.5rem] flex items-center ${className}`}
+    >
+      <Link
+        href="/auth/signup"
+      >
         <div className="flex items-center gap-[0.5rem] md:gap-[0.625rem]">
-            <Image 
-              src={people} 
-              alt='Create Account'
-              className="w-[16px] h-[16px] md:w-[18px] md:h-[18px] lg:w-[20px] lg:h-[20px] inline-block"
-            />
-            <span className='text-[1rem] md:text-[1.125rem] lg:text-[1.25rem] whitespace-nowrap inline-block'>Create Account</span>
+          <Image
+            src={people}
+            alt="Create Account"
+            className="w-[16px] h-[16px] md:w-[18px] md:h-[18px] lg:w-[20px] lg:h-[20px] inline-block"
+          />
+          <span className="text-[1rem] md:text-[1.125rem] lg:text-[1.25rem] whitespace-nowrap inline-block">
+            Create Account
+          </span>
         </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CreateAcc
+export default CreateAcc;
